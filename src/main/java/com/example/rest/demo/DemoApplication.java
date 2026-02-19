e374aa34c4f806ba14dd950ccbf992fcb665aac3
mokesh <mmokesh0@gmail.com>
2026-02-19 15:59:37 +0530

    trick

--- src/main/java/com/example/rest/demo/DemoApplication.java
+++ src/main/java/com/example/rest/demo/DemoApplication.java
@@ -2,10 +2,11 @@ package com.example.rest.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//  //
+
 @SpringBootApplication
+//want
 public class DemoApplication {
-/////
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
