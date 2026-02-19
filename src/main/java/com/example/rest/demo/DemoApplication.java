3c84bb9273e3af1ab0d2140c927cebe4121acf03
mokesh <mmokesh0@gmail.com>
2026-02-19 16:17:49 +0530

    last change

--- src/main/java/com/example/rest/demo/DemoApplication.java
+++ src/main/java/com/example/rest/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//want
+//want to be commit
 public class DemoApplication {
 
     public static void main(String[] args) {
