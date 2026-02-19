6bb6473eced5cf72965ed34fe66fe091f5d6c42f
mokesh <mmokesh0@gmail.com>
2026-02-19 16:21:13 +0530

    third commit

--- src/main/java/com/example/rest/demo/DemoApplication.java
+++ src/main/java/com/example/rest/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//want to be commit
+//want to be commit yes
 public class DemoApplication {
 
     public static void main(String[] args) {
