2855af0b24b72c1bfdce4c4130182b0b252f7004
mokesh <mmokesh0@gmail.com>
2026-02-20 12:29:49 +0530

    more gud changes

--- src/main/java/com/example/rest/demo/DemoApplication.java
+++ src/main/java/com/example/rest/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//want to be commit yes
+//want to be commit yes    i want some changes
 public class DemoApplication {
 
     public static void main(String[] args) {
